a7772841c6707ee2ce6816b16e236134080b5630
antgeo-io <contact.antgeo@gmail.com>
2017-11-13 18:09:00 +0300

    v 0.2.0

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{331:function(n,o){},608:function(n,o,c){n.exports=c(331)}},[608]);
+webpackJsonp([1,2],{399:function(n,o){},936:function(n,o,c){n.exports=c(399)}},[936]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
